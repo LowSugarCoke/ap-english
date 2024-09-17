4c31428b87b0fa9733afa56a398f28a74bb92c7d
LowSugarCoke <jack20499@gmail.com>
2024-09-17 17:14:01 -0600

    fix: fix hero session layout (#2)
    - Fix hero session layout
    - Remove newsletter session
    - TODO: add hero session picture
    - TODO: fix others session layout

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,25 +1,23 @@
 import { Component } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field'; // 匯入 MatFormFieldModule
-import { MatInputModule } from '@angular/material/input'; // 匯入 MatInputModule
-import { MatButtonModule } from '@angular/material/button'; // 匯入 MatButtonModule (如需按鈕)
-import { MatToolbarModule } from '@angular/material/toolbar'; // 匯入 MatToolbarModule
-import { MatIconModule } from '@angular/material/icon'; // 匯入 MatIconModule
-import { MatGridListModule } from '@angular/material/grid-list'; // 匯入 MatGridListModule
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-homepage',
-  standalone: true, // standalone 組件
+  standalone: true,
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
   imports: [
-    MatFormFieldModule, // 匯入 MatFormFieldModule
-    MatInputModule, // 匯入 MatInputModule
-    MatButtonModule, // 匯入 MatButtonModule
-    MatToolbarModule, // 匯入 MatToolbarModule
-    MatIconModule, // 匯入 MatIconModule
-    MatGridListModule, // 匯入 MatGridListModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatGridListModule,
   ],
 })
-export class HomepageComponent {
-  // 組件的邏輯在這裡
-}
+export class HomepageComponent {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { HomepageComponent } from './app/pages/home/home'; // 確保路徑正確
+import { HomepageComponent } from './app/pages/home/home';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -16,10 +16,11 @@ bootstrapApplication(HomepageComponent, {
     provideAnimations(),
     importProvidersFrom(
       BrowserAnimationsModule,
-      MatToolbarModule, // 匯入 Angular Material 的 Toolbar 模組
-      MatButtonModule, // 匯入 Angular Material 的按鈕模組
-      MatIconModule, // 匯入 Angular Material 的 Icon 模組
-      MatGridListModule // 匯入 Angular Material 的 GridList 模組
-    ), provideAnimationsAsync(),
+      MatToolbarModule,
+      MatButtonModule,
+      MatIconModule,
+      MatGridListModule
+    ),
+    provideAnimationsAsync(),
   ],
 }).catch((err) => console.error(err));
