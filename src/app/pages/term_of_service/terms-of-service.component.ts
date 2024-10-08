f7188462cd80aaa5b8db4f74c6ce23856311e9df
Jack Lee <jack20499@gmail.com>
2024-10-07 21:02:35 -0400

    fix: add header and footer in terms_of_service
    - Add header and footer
    - Modify title font size in mobile

--- src/app/pages/term_of_service/terms-of-service.component.ts
+++ src/app/pages/term_of_service/terms-of-service.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { TermsSection } from './terms-of-service-model';
 import { CommonModule } from '@angular/common';
 import { termsSections } from './terms-of-service-data';
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-terms-of-service',
   templateUrl: './terms-of-service.component.html',
   styleUrls: ['./terms-of-service.component.css'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, FooterComponent],
 })
 export class TermsOfServiceComponent {
   termsSections: TermsSection[] = termsSections;
