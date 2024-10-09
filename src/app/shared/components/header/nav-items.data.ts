991e55aded681d8eaf1dcd4909efaaba7ddab0cc
Jack Lee <jack20499@gmail.com>
2024-10-08 20:31:13 -0400

    fix: remove arduino and add apcsp in menu

--- src/app/shared/components/header/nav-items.data.ts
+++ src/app/shared/components/header/nav-items.data.ts
@@ -3,7 +3,7 @@ import { NavItem } from './nav-item.model';
 export const NAV_ITEMS: NavItem[] = [
   { label: 'Home', route: '/', exact: true },
   { label: 'Kaggle', route: '/kaggle' },
-  { label: 'Arduino', route: '/arduino' },
   { label: 'USACO', route: '/usaco' },
   { label: 'APCSA', route: '/apcsa' },
+  { label: 'APCSP', route: '/apcsp' },
 ];
