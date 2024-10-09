e452ea9c21dc1b9fa50fe29860da0d253bdd88a7
LowSugarCoke <jack20499@gmail.com>
2024-10-08 20:47:53 -0400

    refactor: move header and footer in app.component.ts (#24)

--- src/app/pages/term_of_service/terms-of-service.component.ts
+++ src/app/pages/term_of_service/terms-of-service.component.ts
@@ -2,15 +2,13 @@ import { Component } from '@angular/core';
 import { TermsSection } from './terms-of-service-model';
 import { CommonModule } from '@angular/common';
 import { termsSections } from './terms-of-service-data';
-import { HeaderComponent } from '../../shared/components/header/header.component';
-import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-terms-of-service',
   templateUrl: './terms-of-service.component.html',
   styleUrls: ['./terms-of-service.component.css'],
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent],
+  imports: [CommonModule],
 })
 export class TermsOfServiceComponent {
   termsSections: TermsSection[] = termsSections;
